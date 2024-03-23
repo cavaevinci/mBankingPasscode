cf20925e89e5f881e4b7dccf6a0f614043ca6290
cavaevinci <ivanevacicweb+private@gmail.com>
2024-03-23 16:02:03 +0100

    Initial commit

---
+++ mBankingPasscode/mBankingPasscodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  mBankingPasscodeApp.swift
+//  mBankingPasscode
+//
+//  Created by Nino on 23.03.2024..
+//
+
+import SwiftUI
+
+@main
+struct mBankingPasscodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
